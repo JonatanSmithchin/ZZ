e432003ed528cee36e375fd3b99356cf7e1078d6
刘骁 <lxx18912405977@outlook.com>
2022-10-18 21:30:51 +0800

    Merge remote-tracking branch 'origin/lx' into lx ZZ -v0.0.3继续增加了部分GUI

--- src/main/java/ZZ/domain/MessageType.java
+++ src/main/java/ZZ/domain/MessageType.java
@@ -2,7 +2,11 @@ package ZZ.domain;
 
 public interface MessageType {
     final String MESSAGE_LOGIN_SUCCEED = "1";//登录成功
-
     final String MESSAGE_LOGIN_FAILED = "2";//登录失败
-
+    String MESSAGE_COMN_MSG = "3";//普通信息包
+    String MESSAGE_GROUP_MSG = "4";
+    String MESSAGE_GET_ONLINE_USER = "5";//要求返回在线用户列表
+    String MESSAGE_RETURN_ONLINE_USER = "6";
+    String MESSAGE_CLIENT_EXIT = "7";//请求退出
+    String MESSAGE_FILE_MSE = "8";
 }
